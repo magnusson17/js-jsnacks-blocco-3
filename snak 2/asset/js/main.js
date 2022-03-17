286b410d8cdb6f4dab93c6aab1a192d7317f70e4
Ga <gabri.magni.97@gmail.com>
2022-03-17 18:30:30 +0100

    draft

--- snak 2/asset/js/main.js
+++ snak 2/asset/js/main.js
@@ -1,4 +1,19 @@
 let container = document.getElementById("container");
 
 let ask = parseInt(prompt("inserisci un numero"));
-let superList = [];
+let list = [];
+let numRandom;
+
+for (i = 0; i < ask; i++) {
+    numRandom = Math.floor(Math.random() * 100) + 1;
+    for (k = 0; k < 10; k++) {
+        list.push(numRandom);
+        container.innerHTML += `${list[k]}`
+    }
+}
+
+for (let k = 0; k < 10; k++) {
+    
+}
+
+
