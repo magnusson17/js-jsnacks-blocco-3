b879ef56964005449b678e19071f3d1de8dca464
Ga <gabri.magni.97@gmail.com>
2022-03-17 15:12:30 +0100

    snak 1

--- snak 1/asset/js/main.js
+++ snak 1/asset/js/main.js
@@ -1,1 +1,15 @@
 let container = document.getElementById("container");
+let list = [];
+let sum = 0;
+let ask;
+
+while (sum < 50) {
+    ask = parseInt(prompt("inserisci un numero"));
+    if (isNaN(ask)) {
+        alert("ERRORE");
+    } else {
+        sum += ask;
+        list.push(ask);
+    }
+}
+container.innerHTML = `<div>gli elementi del'array sono: ${list}</div><div>la loro somma è: ${sum}</div>`;
