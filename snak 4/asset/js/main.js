ff264c66ceea9b32e929431b621908707da087cf
Ga <gabri.magni.97@gmail.com>
2022-03-17 16:57:08 +0100

    snak 4

--- snak 4/asset/js/main.js
+++ snak 4/asset/js/main.js
@@ -1,15 +1,44 @@
 let container = document.getElementById("container");
 let list = ["Gino", "Pino", "Tizio", "Caio", "Sempronio", "Aldo", "Giovanni", "Giacomo"];
-let sentinel = false;
 
 let ask = prompt("Qual è il tuo nome?");
+let sentinel = false;
 let i = 0;
-while (sentinel = false || i > list.length) {
-    i++;
+
+/*es 1*/
+while (i < list.length) {
     if (ask == list[i]) {
-        sentinel = true;
-        container.innerHTML = `puoi entrare ${ask}`;
+        i = list.length + 1;
+        container.innerHTML = `puoi entrare ${ask}!!!!!!!`;
     } else {
-        container.innerHTML = `non sei in lista`;
+        i++;
     }
 }
+    
+if (i != list.length + 1) {
+    container.innerHTML = `non sei in lista`;
+}
+
+ /*usa for: FUNZIONA MA NON FERMA IL CICLO*/
+// for (i = 0; i < list.length; i++) {
+//     if (ask == list[i]) {
+//         sentinel = true;
+//     }
+// }
+
+// if (sentinel = true) {
+//     container.innerHTML = `puoi entrare ${ask}`;
+// } else {
+//     container.innerHTML = `non puoi entrare`;
+// }
+
+/*es 2: NON FUNZIONA*/
+// do {
+//     if (ask == list[i]) {
+//         sentinel = true;
+//         container.innerHTML = `puoi entrare ${ask}`;
+//     } else {
+//         i++
+//     }
+// } while (i < list.length);
+
