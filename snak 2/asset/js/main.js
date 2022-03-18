247f82d5a6ea3ce4cca3405a22aae74c5b24e85a
Ga <gabri.magni.97@gmail.com>
2022-03-18 10:15:22 +0100

    snak 2 improvement

--- snak 2/asset/js/main.js
+++ snak 2/asset/js/main.js
@@ -1,19 +1,31 @@
 let container = document.getElementById("container");
 
 let ask = parseInt(prompt("inserisci un numero"));
-let list = [];
-let numRandom;
+// let list = [];
+// let numRandom;
 
-for (i = 0; i < ask; i++) {
-    numRandom = Math.floor(Math.random() * 100) + 1;
-    for (k = 0; k < 10; k++) {
-        list.push(numRandom);
-        container.innerHTML += `${list[k]}`
-    }
-}
+// for (i = 0; i < ask; i++) {
+//     numRandom = Math.floor(Math.random() * 100) + 1;
+//     for (k = 0; k < 10; k++) {
+//         list.push(numRandom);
+//         container.innerHTML += `${list[k]}`
+//     }
+// }
 
-for (let k = 0; k < 10; k++) {
+// for (let k = 0; k < 10; k++) {
     
+// }
+
+let i = 0;
+while (i < ask) {
+
+    let newArray = [];
+    for (k = 0; k < 10; k++) {
+        newArray.push(Math.floor(Math.random() * 100) + 1);
+    }
+    console.log(newArray);
+    // container.innerHTML = `${newArray}`;
+    i++;
 }
 
 
